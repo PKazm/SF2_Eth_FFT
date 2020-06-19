c8b6966802d69586e4c23213cc75e601cb59c0cb
PKazm <paulkazmierski@gmail.com>
2020-06-18 23:36:16 -0400

    ignore softconsole p2

--- Softconsole/FFT_Accelerator/Debug/src/ARP_protocol.d
+++
@@ -1,6 +0,0 @@
-src/ARP_protocol.o: ../src/ARP_protocol.c ../src/ARP_protocol.h \
- ../src/../firmware/hal/CortexM3/GNU/cpu_types.h
-
-../src/ARP_protocol.h:
-
-../src/../firmware/hal/CortexM3/GNU/cpu_types.h:
